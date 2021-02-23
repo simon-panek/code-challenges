b1f0450da9e20733286b70c19c5de75fea34b590
Simon Panek <paneks19@gmail.com>
2021-02-23 13:19:12 -0800

    LeetCode 58 accepted

--- _test_/index.test.js
+++ _test_/index.test.js
@@ -1,7 +1,25 @@
-const { hello } = require('../src/index.js');
+const { connectionTest, lengthOfLastWord } = require('../src/index.js');
 
 describe('The index page has the following functions: ', () => {
-  it('The hello function should print the phrase hello world', () => {
-    expect(hello()).toBe(true);
+  it('The connectionTest should return true', () => {
+    expect(connectionTest()).toBe(true);
   })
+
+  it('(LC#58) The lengthOfLastWord function should return 0 if a string with no length or no alpha characters is provided', ()=> {
+    expect(lengthOfLastWord('')).toEqual(0);
+    expect(lengthOfLastWord(' ')).toEqual(0);
+  })
+  it('(LC#58) The lengthOfLastWord function should return the length of the final word in a string', ()=> {
+    expect(lengthOfLastWord('The dog ran fast')).toEqual(4);
+    expect(lengthOfLastWord('one two three')).toEqual(5);
+    expect(lengthOfLastWord('one two three four five six seven eight nine ten')).toEqual(3);
+    expect(lengthOfLastWord('it was a big gray elephant')).toEqual(8);
+    expect(lengthOfLastWord('a')).toEqual(1);
+    expect(lengthOfLastWord(' a b ')).toEqual(1);
+    expect(lengthOfLastWord(' a b  ')).toEqual(1);
+    expect(lengthOfLastWord('   ')).toEqual(0);
+    expect(lengthOfLastWord('thisisareallylongsingleword')).toEqual(27);
+    expect(lengthOfLastWord('     thisisareallylongsingleword')).toEqual(27);
+  })
+
 })
--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 'use strict';
 
-function hello () {
-  console.log('Hello world');
+function connectionTest () {
   return true;
 }
 
+// LeetCode #58
+//Given a string s consists of some words separated by spaces, return the length of the last word in the string. If the last word does not exist, return 0.
+//A word is a maximal substring consisting of non-space characters only.
+//s consists of only English letters and spaces ' '
 
-module.exports = { hello };
+const lengthOfLastWord = (s) => {
+  let length = 0;
+  if (s === '') return length;
+  let arrayOfStrings = s.split(' ');
+  let i = 1;
+  while(arrayOfStrings[arrayOfStrings.length-i].length === 0) {
+    i++;
+    if(i === arrayOfStrings.length + 1) return length = 0;
+  } 
+  length = arrayOfStrings[arrayOfStrings.length-i].length; 
+  return length; 
+};
+
+// Success - Accepted 02/23/2021 13:08
+// Runtime: 76 ms, faster than 79.20% of JavaScript online submissions for Length of Last Word.
+// Memory Usage: 39 MB, less than 9.66% of JavaScript online submissions for Length of Last Word.
+
+module.exports = { connectionTest, lengthOfLastWord };
