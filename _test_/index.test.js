aa1c21c56acab1ca1545a6d1e6911d0fba7c3439
Simon Panek <paneks19@gmail.com>
2021-03-21 10:49:25 -0700

    wrote first test for LC1402

--- _test_/index.test.js
+++ _test_/index.test.js
@@ -63,6 +63,11 @@ describe('The leetCode1402 page has the following functions: ', () => {
   it('The connectionTest3 from leetCode1402 should return true', () => {
     expect(connectionTest3()).toBe(true);
   })
+
+  // it('The dish reducer should return the maximum sum of Like-time that a given input will allow ', ()=> {
+  //   let input = [-1, -8, 0, 5, -9];
+  //   expect(maxSatisfaction(input)).toEqual(14);
+  // })
 })
 
 // End LC#1402
